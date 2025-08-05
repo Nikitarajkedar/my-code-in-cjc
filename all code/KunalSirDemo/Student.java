07f212c4be46dd2244dd35ed57ac4cae6a6bdec7
Nikita Rajkedar <nikitakedar143@gmail.com>
2025-08-05 22:32:48 +0530

    veriable change --

--- all code/KunalSirDemo/Student.java
+++ all code/KunalSirDemo/Student.java
@@ -2,6 +2,7 @@ class Student{
     int id;
     String name;
     float percentage ;
+    String address;
     public static void main(String arg[]){
         System.out.println("hello");
         System.out.println("EvaryOne");
