1f58fc76d0435cbd2c2dcd4e141029b325614c27
Nikita Rajkedar <nikitakedar143@gmail.com>
2025-08-05 23:05:06 +0530

    add syso

--- all code/KunalSirDemo/Student.java
+++ all code/KunalSirDemo/Student.java
@@ -5,9 +5,11 @@ class Student{
     float avg;
     String email;
 
+
     String address;
     public static void main(String arg[]){
         System.out.println("hello");
         System.out.println("EvaryOne");
+        System.out.println("This is Nikita");
     }
 }
