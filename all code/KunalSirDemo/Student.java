00faef5598abc10a5958481d61c028ed0c794083
Nikita Rajkedar <nikitakedar143@gmail.com>
2025-08-05 22:41:52 +0530

    veriable commit

--- all code/KunalSirDemo/Student.java
+++ all code/KunalSirDemo/Student.java
@@ -2,6 +2,7 @@ class Student{
     int id;
     String name;
     float percentage ;
+    float avg;
     String address;
     public static void main(String arg[]){
         System.out.println("hello");
